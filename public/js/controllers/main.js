d9cf3a6ff9bebe39259a0d732e1ae32b5ed37154
adwitz <adam.witzel@gmail.com>
2013-12-26 18:18:58 -0800

    Implement basic recursive dom traversal.

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -2,7 +2,6 @@
 
 angular.module('parserApp')
   .controller('MainCtrl', function ($scope, MainService) {
-    console.log($scope.url);
     $scope.parse = function(){
       console.log(JSON.stringify($scope.url));
       MainService.parser(JSON.stringify($scope.url));
--- routes/parser.js
+++ routes/parser.js
@@ -1,20 +1,47 @@
 'use strict'
 
 var htmlparser = require('htmlparser2');
+var request = require('request');
+
+var concatDom = function(dom){
+  var results = '';
+  var recurse = function(dom){
+    for (var i=0; i<dom.length; i++){
+      if (dom[i].type === 'text' && dom[i].data && dom[i].data !== ' '){
+        results = results.concat(dom[i].data + ' ');
+      }
+      if (dom[i].children){
+        recurse(dom[i].children);
+      }
+    }
+  };
+  recurse(dom);
+  console.log(results);
+  return results;
+};
 
 var handler = new htmlparser.DomHandler(function (error, dom){
   if (error) {
     console.log('parser.js error ln 5', error);
   } else {
+    concatDom(dom);
     console.log('parser.js success ln 7, dom:', dom);
   }
-});
+}, {normalizeWhitespace: true});
 
 var parser = new htmlparser.Parser(handler);
 
 exports.parser = function(req, res){
-  console.log(JSON.parse(req.body.url));
-  parser.write(JSON.parse(req.body.url));
-  res.send(JSON.parse(req.body.url));
-  parser.done();
+  request(JSON.parse(req.body.url), function(error, response, body){
+    if(!error && response.statusCode == 200){
+      parser.write(body);
+      parser.done();
+    } else if (error) {
+      console.log('there appears to be an error', error);
+    }
+  });
 };
+
+//write function to plug into handler success function
+//this will be the main algorithm
+
